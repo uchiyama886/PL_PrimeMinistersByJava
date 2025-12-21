5aa5f597c049e682ddacc3f7aa5b1c3b562466d7
OgamiRikuto <gami6322368@gmail.com>
2025-12-21 23:30:47 +0900

    fix;スケルトンのソースコードに変更

--- primeministers/Downloader.java
+++ primeministers/Downloader.java
@@ -1,43 +1,70 @@
-package primeministers;
+package csv2html;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.List;
+import utility.ImageUtility;
 
 /**
  * ダウンローダ：CSVファイル・画像ファイル・サムネイル画像ファイルをダウンロードする。
  */
-public class Downloader extends IO {
+public class Downloader extends IO
+{
+	/**
+	 * ダウンローダのコンストラクタ。
+	 * @param aTable テーブル
+	 */
+	public Downloader(Table aTable)
+	{
+		super(aTable);
+
+		return;
+	}
 
 	/**
 	 * 総理大臣の情報を記したCSVファイルをダウンロードする。
 	 */
-	public void downloadCSV() {
-
+	public void downloadCSV()
+	{
+		return;
 	}
 
 	/**
 	 * 総理大臣の画像群をダウンロードする。
 	 */
-	public void downloadImages() {
+	public void downloadImages()
+	{
+		int indexOfImage = this.attributes().indexOfImage();
+		this.downloadPictures(indexOfImage);
 
+		return;
 	}
 
 	/**
 	 * 総理大臣の画像群またはサムネイル画像群をダウンロードする。
+	 * @param indexOfPicture 画像のインデックス
 	 */
-	private void downloadPictures() {
-
+	private void downloadPictures(int indexOfPicture)
+	{
+		return;
 	}
 
 	/**
 	 * 総理大臣の画像群をダウンロードする。
 	 */
-	public void downloadThumbnails() {
+	public void downloadThumbnails()
+	{
+		int indexOfThumbnail = this.attributes().indexOfThumbnail();
+		this.downloadPictures(indexOfThumbnail);
 
+		return;
 	}
 
 	/**
-	 * 総理大臣の情報を記したCSVファイルをダウンロードして、画像群やサムネイル画像群もダウロードする。
+	 * 総理大臣の情報を記したCSVファイルをダウンロードして、画像群やサムネイル画像群もダウンロードする。
 	 */
-	public void perform() {
-
+	public void perform()
+	{
+		return;
 	}
-
 }
--- primeministers/Tuple.java
+++ primeministers/Tuple.java
@@ -1,12 +1,12 @@
-package primeministers;
+package csv2html;
 
 import java.util.List;
 
 /**
  * タプル：情報テーブルの中の各々のレコード。
  */
-public class Tuple extends Object {
-
+public class Tuple extends Object
+{
 	/**
 	 * 属性リストを記憶するフィールド。
 	 */
@@ -17,34 +17,59 @@ public class Tuple extends Object {
 	 */
 	private List<String> values;
 
-	public Tuple(Attributes instanceOfAttributes, List<String> valueCollection) {
+	/**
+	 * 属性リストと値リストからタプルを作るコンストラクタ。
+	 * @param instanceOfAttributes 属性リスト
+	 * @param valueCollection 値リスト
+	 */
+	public Tuple(Attributes instanceOfAttributes, List<String> valueCollection)
+	{
 		super();
 
 		this.attributes = instanceOfAttributes;
 		this.values = valueCollection;
-		
+
 		return;
 	}
 
 	/**
 	 * 属性リストを応答する。
+	 * @return 属性リスト
 	 */
-	public Attributes attributes() {
-		return attributes;
+	public Attributes attributes()
+	{
+		return this.attributes;
 	}
 
 	/**
 	 * 自分自身を文字列にして、それを応答する。
+	 * @return 自分自身の文字列
 	 */
-	public String toString() {
-		return null;
+	public String toString()
+	{
+		StringBuffer aBuffer = new StringBuffer();
+		Class<?> aClass = this.getClass();
+		aBuffer.append(aClass.getName());
+		aBuffer.append("[");
+		int index = 0;
+		for (String aString : this.values())
+		{
+			if (index != 0) { aBuffer.append(","); }
+			aBuffer.append(this.attributes().at(index));
+			aBuffer.append("(" + aString + ")");
+			index++;
+		}
+		aBuffer.append("]");
+
+		return aBuffer.toString();
 	}
 
 	/**
 	 * 値リストを応答する。
+	 * @return 値リスト
 	 */
-	public List<String> values() {
-		return values;
+	public List<String> values()
+	{
+		return this.values;
 	}
-
 }
