e4fe77c80c3b34f0cd57e006cd92cf9d9634bfc9
OgamiRikuto <gami6322368@gmail.com>
2025-12-23 05:22:37 +0900

    完成

--- primeministers/Reader.java
+++ primeministers/Reader.java
@@ -2,7 +2,6 @@ package primeministers;
 
 import condition.Condition;
 import condition.Interval;
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.function.Consumer;
 import java.util.HashMap;
